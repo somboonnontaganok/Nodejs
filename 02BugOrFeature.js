9d7f72a3da41f2004e07645edc18da50b56a7949
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-04-25 11:46:48 +0700

    change code function

--- 02BugOrFeature.js
+++ 02BugOrFeature.js
@@ -1,36 +1,13 @@
-console.log('Node.js is running...')
-
-
-//Is it a feature ot bug
-// function queryDatabase()  {
-//     setTimeout(function() {
-//         var r = 'Finish QueryDatabase()'
-//         return r
-//  },2000)
-// }
-
-// function main() {
-//         console.log('Result: ' + queryDatabase());    
-// }
-
-// main()
-
-
-
-
-
 //Back Up Code 
-
-function queryDatabase(callback)  {
+var main = function(r) {
+    console.log('Result: ' + r);     
+};
+var queryDatabase = function(callback)  {
     setTimeout(function() {
         var r = 'Finish QueryDatabase()'
         callback(r)
  },2000)
-}
+};
 
-function main() {
-    queryDatabase(function(r){
-        console.log('Result: ' + r);
-    })
-     
-}
+console.log('Wait for set time out');
+queryDatabase(main);
--- 03HelloWorldNode.js
+++ 03HelloWorldNode.js
@@ -3,5 +3,6 @@ var http = require('http');
 // Module HTTP
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  
   res.end('Hello World!');
-}).listen(8080);
+}).listen(80);

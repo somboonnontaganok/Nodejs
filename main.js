0ea52d7ca5014bd1c79ecbd1ee2a15bd0f9cd9f6
Somboon Nontaganok <125272647+somboonnontaganok@users.noreply.github.com>
2023-04-25 11:19:57 +0700

    non Blocking (main.js)

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var fs = require("fs");
-var data = fs.readFileSync('input.txt');
+fs.readFile('input.txt', function(err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+});
 
-console.log(data.toString());
+// console.log(data.toString());
 console.log("Prgram Ended");

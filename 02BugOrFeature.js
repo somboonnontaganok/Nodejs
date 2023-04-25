2ad995c1f8e52adff1443877806005f71c3ae663
somboonnontaganok <somboonnontaganok@gmail.com>
2023-04-25 21:46:23 +0700

    finish02

--- 02BugOrFeature.js
+++ 02BugOrFeature.js
@@ -2,26 +2,7 @@ console.log('Node.js is running...')
 
 
 //Is it a feature ot bug
-function queryDatabase()  {
-    setTimeout(function() {
-        var r = 'Finish QueryDatabase()'
-        return r
- },2000)
-}
-
-function main() {
-        console.log('Result: ' + queryDatabase());    
-}
-
-main()
-
-
-
-
-
-//Back Up Code 
-
-// function queryDatabase(callback)  {
+// function queryDatabase()  {
 //     setTimeout(function() {
 //         var r = 'Finish QueryDatabase()'
 //         callback(r)
@@ -29,8 +10,29 @@ main()
 // }
 
 // function main() {
-//     queryDatabase(function(r){
-//         console.log('Result: ' + r);
-//     })
-     
+//         // console.log('Result: ' + queryDatabase(r));
+//         queryDatabase(function (r) {
+//             console.log('Result is: ' + r)
+//         })    
 // }
+
+
+
+
+
+
+//Back Up Code 
+
+function queryDatabase(callback)  {
+    setTimeout(function() {
+        var r = 'Finish QueryDatabase()'
+        callback(r)
+ },2000)
+}
+
+function main() {
+    queryDatabase(function(r){
+        console.log('Result: ' + r);
+    })
+     
+}
